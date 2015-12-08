d0cb6716756249c011c95f0d0069694ebee54c11
tgaff <tgaff@alumni.nd.edu>
2015-12-07 20:44:07 -0800

    Sprint 1 Step 5: GET /api/albums connected to database

--- server.js
+++ server.js
@@ -4,46 +4,16 @@
 var express = require('express');
 // generate a new express app and call it 'app'
 var app = express();
-
+var mongoose = require('mongoose');
 // serve static files from public folder
 app.use(express.static(__dirname + '/public'));
+
 
 /************
  * DATABASE *
  ************/
 
-/* hard-coded data */
-var albums = [];
-albums.push({
-              _id: 132,
-              artistName: 'Nine Inch Nails',
-              name: 'The Downward Spiral',
-              releaseDate: '1994, March 8',
-              genres: [ 'industrial', 'industrial metal' ]
-            });
-albums.push({
-              _id: 133,
-              artistName: 'Metallica',
-              name: 'Metallica',
-              releaseDate: '1991, August 12',
-              genres: [ 'heavy metal' ]
-            });
-albums.push({
-              _id: 134,
-              artistName: 'The Prodigy',
-              name: 'Music for the Jilted Generation',
-              releaseDate: '1994, July 4',
-              genres: [ 'electronica', 'breakbeat hardcore', 'rave', 'jungle' ]
-            });
-albums.push({
-              _id: 135,
-              artistName: 'Johnny Cash',
-              name: 'Unchained',
-              releaseDate: '1996, November 5',
-              genres: [ 'country', 'rock' ]
-            });
-
-
+var db = require('./models');
 
 /**********
  * ROUTES *
@@ -74,7 +44,9 @@ app.get('/api', function api_index (req, res){
 });
 
 app.get('/api/albums', function albumsIndex(req, res) {
-  res.json(albums);
+  db.Album.find({}, function(err, albums) {
+    res.json(albums);
+  });
 });
 
 
