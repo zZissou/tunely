24a02be7cf2b25c918f070384c98a5b47010e100
tgaff <tgaff@alumni.nd.edu>
2015-12-07 22:43:33 -0800

    Sprint 3 Step 6: server saves song

--- models/index.js
+++ models/index.js
@@ -1,5 +1,7 @@
 var mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost/tunely_test");
 var Album = require('./album');
+var Song = require('./song');
 
 module.exports.Album = Album;
+module.exports.Song = Song;
