6e9b0c6457898f52e80a2c20e73625a89f6561c7
tgaff <tgaff@alumni.nd.edu>
2015-12-07 23:02:33 -0800

    Sprint 3 Step 7

--- server.js
+++ server.js
@@ -67,6 +67,14 @@ app.post('/api/albums', function albumCreate(req, res) {
 });
 
 
+app.get('/api/albums/:id', function albumShow(req, res) {
+  console.log('requested album id=', req.params.id);
+  db.Album.findOne({_id: req.params.id}, function(err, album) {
+    res.json(album);
+  });
+});
+
+
 app.post('/api/albums/:albumId/songs', function songsCreate(req, res) {
   console.log('body', req.body);
   db.Album.findOne({_id: req.params.albumId}, function(err, album) {
